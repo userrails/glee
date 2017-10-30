4c8da5c9c8c0b318560f99ae64deb0c0f6f81183
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-10-30 10:27:31 +0545

    autoload migrations

--- lib/glee/engine.rb
+++ lib/glee/engine.rb
@@ -1,5 +1,11 @@
 module Glee
   class Engine < ::Rails::Engine
     isolate_namespace Glee
+
+    initializer "glee", before: :load_config_initializers do |app|
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+          Rails.application.config.paths["db/migrate"] << expanded_path
+      end
+    end
   end
 end
